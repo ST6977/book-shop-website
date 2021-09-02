7e556a968a689fc52e93ef2ac0af4c45ce0e3b8b
ST6977 <sumaiyat610@gmail.com>
2021-09-02 20:55:42 +0600

    everything added

--- book.js
+++ book.js
@@ -1,40 +1,68 @@
-const searchBook = () => {
-  const searchField = document.getElementById('search-field');
-  const searchText = searchField.value;
-  searchField.value = '';
-  const url = `https://openlibrary.org/search.json?q=${searchText}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      if (data.docs.length > 0) {
-        document.getElementById('error').classList.add("d-none");
-        displayDocs(data.docs)
-      } else {
-        document.getElementById('error').classList.remove("d-none");
-        document.getElementById('search-result').textContent = ``
-      }
-    });
+//load book data from the api
+const loadBookData = () => {
+  const searchText = document.getElementById('input-field').value;
+  document.getElementById('input-field').value = '';
+  document.getElementById('spinner').classList.remove('d-none');
+
+  //when user dont give any input
+  if (searchText === '') {
+      document.getElementById('spinner').classList.add('d-none');
+      const searchResult = document.getElementById('search-result');
+      searchResult.textContent = '';
+      searchResult.innerHTML = `
+      <h1 class="bg-warning w-75 mx-auto rounded-3 text-center p-2">Please, enter a book name</h1>
+      `
+  }
+
+  //when user give input
+  else {
+      const url = `https://openlibrary.org/search.json?q=${searchText}`;
+      fetch(url)
+          // console.log(url)
+          .then(res => res.json())
+          .then(data => displayBookData(data))
+  }
+
 }
-const displayDocs = docs => {
-  const searchResult = document.getElementById('search-result')
 
-  docs.forEach(doc => {
-    console.log(doc);
-    const div = document.createElement('div')
-    div.classList.add('col');
-    div.innerHTML = `
-   <div class="col">
-   <div class="card">
-     <img src= " https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg"  class="card-img-top " >
-     <div class="card-body">
-       <p class="card-title">Author name ${doc.author_name}</p>
-       <p class="card-text">First Publish Year ${doc.first_publish_year}</p>
-     </div>
-   </div>
- </div> `;
-    searchResult.appendChild(div)
-  })
+const displayBookData = data => {
+  const booksInfo = data.docs.slice(0, 21);
+  document.getElementById('spinner').classList.add('d-none');
+  const searchResult = document.getElementById('search-result');
+  document.getElementById('result-found').innerText = `Books Found: ${data.numFound}`;
+  searchResult.textContent = '';
 
+  //if there is no book in the search item
+  if (booksInfo.length === 0) {
+      searchResult.innerHTML = `
+      
+     <h1 class="bg-warning w-75 mx-auto rounded-3 text-center p-2">Sorry, No search results found!!</h1>
+
+      `
+  }
+  else {
+
+      booksInfo.forEach(bookInfo => {
+          // console.log(bookInfo)
+          const div = document.createElement('div');
+          div.classList.add('col');
+          div.innerHTML = `
+          <div class="card shadow-sm">
+              <div class="d-flex justify-content-center mt-3">
+              <img src="https://covers.openlibrary.org/b/id/${bookInfo.cover_i}-M.jpg" height=150px width=130px  alt="Book image">
+              </div>
+              <div class="card-body">
+                  <h5 class="card-title text-center mb-4">${bookInfo.title}</h5>
+                  
+                  <p class="mb-0"><b>First Publish Year: </b>${bookInfo.first_publish_year ? bookInfo.first_publish_year : 'Not Found'}</p>
+                  <p class="mb-0"><b>Author: </b>${bookInfo.author_name[0] ? bookInfo.author_name[0] : 'Not Found'}</p>
+                  <p class="mb-0"><b>Publisher: </b>${bookInfo.publisher[0] ? bookInfo.publisher[0] : 'Not Found'}</p>
+              </div>
+          </div>
+          `
+          searchResult.appendChild(div);
+      })
+  }
 }
      
 
